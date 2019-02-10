e52ba65bf9d4d0e7efceceaf59ce77cb95cf0f5e
HR <hrui1990@gmail.com>
2019-02-10 17:59:39 -0600

    finally fixed webpack

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  // console.log("You can see this in the terminal");
+  // console.log will output to the console
+  const rui = { name: "Rui", familiar: "Mochi", cool: true };
+  // res.send("Hey! It works!");
+
+  res.json(rui);
+  // use .json method
+
+  // res.send(req.query.name);
+  // use this for get request parameter
+
+  // res.json(req.query);
+  // better yet, return as get request as json
 });
 
 module.exports = router;
